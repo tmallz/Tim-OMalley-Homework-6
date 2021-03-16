0a5d72f90e93dbe04eb074a6cc787626fc512213
Tim O'Malley <tomalley24@gmail.com>
2021-03-16 12:26:53 -0500

    added forcast cards and logic

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,30 +1,55 @@
 var currentDay;
 var searchEl = document.querySelector('#submit');
 var dateDisplayEl = document.querySelector('#date-display');
+var iconDisplayEl = document.querySelector('#icon-display');
 var cityNameEl = document.querySelector('#city-name');
 var searchInputEl = document.querySelector('#searchCity');
 var temperatureEL = document.querySelector('#temperature');
 var humidityEl = document.querySelector('#humidity')
 var windEl = document.querySelector('#wind-speed');
-var uvIndexEl = document.querySelector('#uv-index')
+var uvIndexEl = document.querySelector('#uv-index');
+var forecastIconEl1 = document.querySelector('#forecastIcon1');
+var forecastTempEl1 = document.querySelector('#forecastTemp1');
+var forecastHumidEl1 = document.querySelector('#forecastHumid1');
+var forecastIconEl2 = document.querySelector('#forecastIcon2');
+var forecastTempEl2 = document.querySelector('#forecastTemp2');
+var forecastHumidEl2 = document.querySelector('#forecastHumid2');
+var forecastIconEl3 = document.querySelector('#forecastIcon3');
+var forecastTempEl3 = document.querySelector('#forecastTemp3');
+var forecastHumidEl3 = document.querySelector('#forecastHumid3');
+var forecastIconEl4 = document.querySelector('#forecastIcon4');
+var forecastTempEl4 = document.querySelector('#forecastTemp4');
+var forecastHumidEl4 = document.querySelector('#forecastHumid4');
+var forecastIconEl5 = document.querySelector('#forecastIcon5');
+var forecastTempEl5 = document.querySelector('#forecastTemp5');
+var forecastHumidEl5 = document.querySelector('#forecastHumid5');
 var searchButtonsEl = document.querySelector('.search-results-buttons');
+var forecastDateEl1 = document.querySelector('#forecastDate1');
+var forecastDateEl2 = document.querySelector('#forecastDate2');
+var forecastDateEl3 = document.querySelector('#forecastDate3');
+var forecastDateEl4 = document.querySelector('#forecastDate4');
+var forecastDateEl5 = document.querySelector('#forecastDate5');
 var latitude;
 var longitude;
 var urlName;
+var forecastDateArray = [forecastDateEl1, forecastDateEl2, forecastDateEl3, forecastDateEl4, forecastDateEl5];
+var forecastIconArray = [forecastIconEl1, forecastIconEl2, forecastIconEl3,  forecastIconEl4,  forecastIconEl5];
+var forecastTempArray = [forecastTempEl1, forecastTempEl2,  forecastTempEl3,forecastTempEl4, forecastTempEl5];
+var forecastHumidArray =[forecastHumidEl1, forecastHumidEl2, forecastHumidEl3, forecastHumidEl4, forecastHumidEl5];
 
 
 function getWeatherFetch(cityNameParam){
-   var weatherUrl = "https://cors-anywhere.herokuapp.com/http://api.openweathermap.org/data/2.5/weather?q=" + cityNameParam + "&appid=42a22477232e5d6f0ba0202c57096ead";
+   var weatherUrl = "http://api.openweathermap.org/data/2.5/weather?q=" + cityNameParam + "&units=imperial&appid=42a22477232e5d6f0ba0202c57096ead";
    return weatherUrl;
 }
 
 function getForecastFetch(cityNameParam){
-    var forecastUrl = "https://cors-anywhere.herokuapp.com/http://api.openweathermap.org/data/2.5/forecast?q=" + cityNameParam + "&appid=42a22477232e5d6f0ba0202c57096ead";
+    var forecastUrl = "http://api.openweathermap.org/data/2.5/forecast?q=" + cityNameParam + "&units=imperial&appid=42a22477232e5d6f0ba0202c57096ead";
     return forecastUrl;
 }
 
 function getUvIndexFetch(latParam, longParam){
-    var uvIndexUrl = "https://cors-anywhere.herokuapp.comhttp://api.openweathermap.org/data/2.5/uvi?lat=" + latParam + "&lon=" + longParam + "&appid=42a22477232e5d6f0ba0202c57096ead";
+    var uvIndexUrl = "http://api.openweathermap.org/data/2.5/uvi?lat=" + latParam + "&lon=" + longParam + "&appid=42a22477232e5d6f0ba0202c57096ead";
     return uvIndexUrl;
 }
 
@@ -39,21 +64,24 @@ function init(){
 
 function displayCurrentWeather(dataParam){
     //Dynamically changes the name to searched city and resets the date
-    cityNameEl.textContent = searchInputEl.value + " " + "(" + currentDay + ")";
-    longitude = dataParam.coord.lon;
-    latitude = dataParam.coord.lat;
-    console.log(longitude, latitude);
-    uvIndexUrl = getUvIndexFetch(latitude, longitude);
+    var currentConditions = dataParam.weather;
+    var weatherIcon;
+    if(currentConditions[0].main === "Clouds"){
+     // weatherIcon = ;
+    }
 
-    var tempCels = Math.round(((dataParam.main.temp) - 273.15));
-    var tempF = (tempCels * (9/5)) + 32;
-    temperatureEL.textContent = tempF;
+
+    cityNameEl.textContent = searchInputEl.value + " " + "(" + currentDay + ")"// + " " + weatherIcon;
+    console.log(currentConditions[0].main)
+
+
+    temperatureEL.textContent = dataParam.main.temp;
     humidityEl.textContent = dataParam.main.humidity;
     windEl.textContent = dataParam.wind.speed;
 }
 
 function displayUvIndex(dataParamUv){
-    //uvIndexEl.textContent = 
+    uvIndexEl.textContent = dataParamUv.value;
 }
 
 function makeWeatherFetch(urlNameParam){
@@ -64,6 +92,10 @@ function makeWeatherFetch(urlNameParam){
         response.json().then(function (data) {
           console.log(data);
           displayCurrentWeather(data);
+          longitude = data.coord.lon;
+          latitude = data.coord.lat;
+          uvUrl = getUvIndexFetch(latitude, longitude);
+          makeUvIndexFetch(uvUrl);
         });
       } else {
         alert('Error: ' + response.statusText);
@@ -92,6 +124,42 @@ function makeUvIndexFetch(urlNameParam){
     });
 }
 
+function displayForecast(forecastDataParam){
+  var tempConditions = forecastDataParam.list;
+  forecastDateArray[0].textContent = tempConditions[0].dt_txt;
+  forecastTempArray[0].textContent = tempConditions[0].main.temp;
+  forecastHumidArray[0].textContent = tempConditions[0].main.humidity;
+  forecastDateArray[1].textContent = tempConditions[8].dt_txt;
+  forecastTempArray[1].textContent = tempConditions[8].main.temp;
+  forecastHumidArray[1].textContent = tempConditions[8].main.humidity;
+  forecastDateArray[2].textContent = tempConditions[16].dt_txt;
+  forecastTempArray[2].textContent = tempConditions[16].main.temp;
+  forecastHumidArray[2].textContent = tempConditions[16].main.humidity;
+  forecastDateArray[3].textContent = tempConditions[24].dt_txt;
+  forecastTempArray[3].textContent = tempConditions[24].main.temp;
+  forecastHumidArray[3].textContent = tempConditions[24].main.humidity;
+  forecastDateArray[4].textContent = tempConditions[32].dt_txt;
+  forecastTempArray[4].textContent = tempConditions[32].main.temp;
+  forecastHumidArray[4].textContent = tempConditions[32].main.humidity;
+}
+
+function makeForecastFetch(forecastUrlParam){
+  fetch(forecastUrlParam)
+  .then(function (response) {
+    if (response.ok) {
+      response.json().then(function (data) {
+        console.log(data);
+        displayForecast(data);
+      });
+    } else {
+      alert('Error: ' + response.statusText);
+    }
+  })
+  .catch(function (error) {
+    alert('Unable to connect to openweatherapp');
+  });
+}
+
 function makeCityButton(){
     var newButton = document.createElement('a');
     newButton.classList = 'waves-effect waves-light btn';
@@ -109,7 +177,7 @@ var handleformsubmit = function(event){
     uvUrl = getUvIndexFetch(latitude, longitude); 
     forecastUrl = getForecastFetch(cityName);
     makeWeatherFetch(urlName);
-    makeUvIndexFetch(uvUrl);
+    makeForecastFetch(forecastUrl);
     makeCityButton(cityName);
 }
 
